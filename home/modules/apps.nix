0e6ab88889f75bfa1be26451529a7b94dcaafe4e
saponace <saponace@gmail.com>
2026-05-03 19:49:36 -0400

    alacritty

--- home/modules/apps.nix
+++ home/modules/apps.nix
@@ -6,7 +6,32 @@
     vlc
   ];
 
-  programs.firefox.enable = true;
+  programs = {
+    firefox.enable = true;
+
+    alacritty = {
+      enable = true;
+      theme = "monokai";
+      settings = {
+        window.opacity = 0.8;
+        font = {
+          size = 10;
+          normal = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "JetBrainsMono Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "JetBrainsMono Nerd Font";
+            style = "italic";
+          };
+        };
+      };
+    };
+  };
 
   services = {
     pasystray.enable = true;
