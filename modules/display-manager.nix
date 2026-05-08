99214ef73fa32810988f2d1dfa424f75e0ab6751
saponace <saponace@gmail.com>
2026-05-07 23:55:24 -0400

    add theme to sddm

--- modules/display-manager.nix
+++ modules/display-manager.nix
@@ -1,10 +1,17 @@
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "catppuccin-mocha-mauve";
   };
 
+  environment.systemPackages = [ 
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      accent = "mauve";
+    })
+  ];
 }
