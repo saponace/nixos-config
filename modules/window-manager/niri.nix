06f81d73afa7f418bba81c7d070602d7456005f6
saponace <saponace@gmail.com>
2026-05-10 00:08:17 -0400

    niri config - first draft

--- modules/window-manager/niri.nix
+++ modules/window-manager/niri.nix
@@ -1,7 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.niri.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    fuzzel # app launcher
+    swaylock # app launcher
+    waybar
+  ];
 
   home-manager.users.saponace.home.file.".config/niri/config.kdl" = {
     source = ./config.kdl;
