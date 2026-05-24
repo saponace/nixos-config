07287c1f4d8fc67efd29a4a14a6676ad234415a7
saponace <saponace@gmail.com>
2026-05-24 01:56:14 -0400

    udisks

--- modules/base/services.nix
+++ modules/base/services.nix
@@ -4,4 +4,6 @@
   services.openssh.enable = true;
   services.printing.enable = true;
   services.spice-vdagentd.enable = true;  # Enables clipboard sharing with host when running as vm
+  services.udisks2.enable = true;
+  security.polkit.enable = true;  # Policy kit
 }
