d6ee28246eb8481b1cfcfa6cc09911f8126487a5
saponace <saponace@gmail.com>
2026-05-30 21:09:06 -0400

    topinambour: attempt to fix stak

--- hosts/topinambour/network.nix
+++ hosts/topinambour/network.nix
@@ -8,23 +8,11 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true;
+    hostName = "stak"; # advertise as stak.local (A record, more reliable than CNAME aliases)
     publish = {
       enable = true;
       addresses = true;
       domain = true;
     };
-    # Publish "stak.local" as a CNAME alias so other configs can use it without knowing the real hostname
-    extraServiceFiles.stak = ''
-      <?xml version="1.0" standalone='no'?>
-      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-      <service-group>
-        <name>stak</name>
-        <service>
-          <type>_device-info._tcp</type>
-          <port>0</port>
-        </service>
-        <host-name>stak.local</host-name>
-      </service-group>
-    '';
   };
 }
