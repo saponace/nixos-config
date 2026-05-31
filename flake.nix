23d6c88c396cc8b58de0055a56b8c536d4ad4c4f
saponace <saponace@gmail.com>
2026-05-30 20:50:27 -0400

    topinambour: fix untrusted flake config warning

--- flake.nix
+++ flake.nix
@@ -1,13 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nixos-raspberrypi.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -23,6 +23,9 @@
         "flakes"
       ];
       trusted-users = [ "root" username ];
+      # Declared here rather than flake.nix nixConfig to avoid --accept-flake-config warnings
+      extra-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
+      extra-trusted-public-keys = [ "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" ];
     };
 
     nixpkgs.config.allowUnfreePredicate = pkg:
