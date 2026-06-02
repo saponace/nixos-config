34c6ea2ee3d4cbe98003018069683917aa6b67b7
saponace <saponace@gmail.com>
2026-06-01 22:13:34 -0400

    move vdagent to vm

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -11,6 +11,8 @@
 
   networking.hostName = "vm";
 
+  services.spice-vdagentd.enable = true; # Enables clipboard sharing with host when running as vm
+
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
--- modules/desktop/services.nix
+++ modules/desktop/services.nix
@@ -2,7 +2,6 @@
 
 {
   services.printing.enable = true;
-  services.spice-vdagentd.enable = true;  # Enables clipboard sharing with host when running as vm
   services.udisks2.enable = true;
   security.polkit.enable = true;  # Policy kit
 }
