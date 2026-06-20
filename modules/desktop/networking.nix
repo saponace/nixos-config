a5866efe2c1193e903b425c9fa4c219cbc398651
saponace <saponace@gmail.com>
2026-06-20 11:46:04 -0400

    Revert "fix avahi intermittent error"
    This reverts commit 01ff159e31b9141af157cf37a2652d33bc5b2e1c.

--- modules/desktop/networking.nix
+++ modules/desktop/networking.nix
@@ -5,14 +5,8 @@
   services.blueman.enable = true; # Bluetooth audio
 
   networking = {
-    networkmanager = {
-      enable = true;
-      insertNameservers = [ "1.1.1.1" ];
-    };
-    # DNS is owned by NetworkManager. Avoid networking.nameservers/resolvconf
-    # because that "static" path pushes resolv.conf through openresolv, whose avahi
-    # hook fails network-local-commands at boot.
-    resolvconf.enable = false;
+    networkmanager.enable = true;
+    nameservers = [ "1.1.1.1" ];
   };
 
   services.avahi = {
