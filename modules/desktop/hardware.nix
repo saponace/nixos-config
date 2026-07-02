cb51c7dabf00584738e0e67bc6dddd2b9ff95841
saponace <saponace@gmail.com>
2026-07-01 23:04:09 -0400

    wip disko-intall

--- modules/desktop/hardware.nix
+++ modules/desktop/hardware.nix
@@ -3,7 +3,11 @@
 {
   hardware.i2c.enable = true;
 
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt = {
+    emulatedSystems = [ "aarch64-linux" ];
+    # Static qemu so emulation works in chroots
+    preferStaticEmulators = true;
+  };
 
   users.users.${username}.extraGroups = [ "i2c" ];
 }
