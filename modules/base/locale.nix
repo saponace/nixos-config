79a5905badfe4c937452bdef6435b85ee7bb5600
saponace <saponace@gmail.com>
2026-07-15 00:29:58 -0400

    proper cedilla dead key

--- modules/base/locale.nix
+++ modules/base/locale.nix
@@ -1,7 +1,15 @@
-_:
+{ username, ... }:
 
 {
   time.timeZone = "America/Montreal";
   i18n.defaultLocale = "en_US.UTF-8";
   console.useXkbConfig = true; # derives console keymap from services.xserver.xkb.layout
+
+  # Override dead key bindings
+  home-manager.users.${username}.home.file.".XCompose".text = ''
+    include "%L"
+
+    <dead_acute> <c> : "ç"
+    <dead_acute> <C> : "Ç"
+  '';
 }
